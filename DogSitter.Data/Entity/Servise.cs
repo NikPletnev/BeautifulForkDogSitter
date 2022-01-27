bc31b3761ddae28f91b76fb0d2398fe413d5c2be
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-01-27 17:21:46 +0300

    add servise

---
+++ DogSitter.Data/Entity/Servise.cs
@@ -0,0 +1,12 @@
+﻿namespace DogSitter.DAL.Entity
+{
+    public class Servise
+    {
+        public int Id { get; set; }
+        public Order Order { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public double DurationHours { get; set; }
+    }
+}
