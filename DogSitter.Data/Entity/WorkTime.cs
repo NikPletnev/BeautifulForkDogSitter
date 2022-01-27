db3b6e24bb305c8ec49fd8aba232c454ad182ec5
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-01-27 18:01:04 +0300

    fix smth

--- DogSitter.Data/Entity/WorkTime.cs
+++ DogSitter.Data/Entity/WorkTime.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DogSitter.DAL.Entity
+﻿namespace DogSitter.DAL.Entity
 {
-    internal class WorkTime
+    public class WorkTime
     {
         public int Id { get; set; }
         public DateTime DateStart { get; set; }
