f7d37bf05b8d65614f88aa6ee778fa549688b728
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-01-27 17:58:47 +0300

    add worktime

---
+++ DogSitter.Data/Entity/Enums.cs
@@ -0,0 +1,5 @@
+﻿namespace DogSitter.DAL.Entity
+{
+    public enum Weekdays { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Satturday }
+
+}
---
+++ DogSitter.Data/Entity/WorkTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSitter.DAL.Entity
+{
+    internal class WorkTime
+    {
+        public int Id { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateClouse { get; set; }
+        public Weekdays Weekdays { get; set; }
+    }
+}
