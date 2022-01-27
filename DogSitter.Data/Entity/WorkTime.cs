c4c4cf424e8afd51d8dc098305eb59935193b9d5
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-01-27 18:15:11 +0300

    add sitter in work time

--- DogSitter.Data/Entity/WorkTime.cs
+++ DogSitter.Data/Entity/WorkTime.cs
@@ -3,6 +3,7 @@
     public class WorkTime
     {
         public int Id { get; set; }
+        public Sitter Sitter { get; set; }
         public DateTime DateStart { get; set; }
         public DateTime DateClouse { get; set; }
         public Weekdays Weekdays { get; set; }
