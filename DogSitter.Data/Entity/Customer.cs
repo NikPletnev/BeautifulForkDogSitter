7a220c22b61793c6fa72ec25218660dac353bd49
Nikita <klophah@gmail.com>
2022-01-28 14:11:27 +0300

    Customer class added

---
+++ DogSitter.Data/Entity/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSitter.DAL.Entity
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public Login? Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Email ContactEmail { get; set; }
+        public Phone ContactPhone { get; set; }
+        public List<Dog> Dogs { get; set; }
+        public List<Sitter> {get; set;}
+        public Address Address { get; set; }
+
+    }
+}
