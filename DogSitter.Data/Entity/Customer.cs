0ba9ff1b9f394bcac0539ccd7742321af0113136
Nikita <klophah@gmail.com>
2022-01-28 14:52:00 +0300

    Added address class

--- DogSitter.Data/Entity/Customer.cs
+++ DogSitter.Data/Entity/Customer.cs
@@ -9,12 +9,12 @@ namespace DogSitter.DAL.Entity
     public class Customer
     {
         public int Id { get; set; }
-        public Login? Login { get; set; }
+        public string Login { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Email ContactEmail { get; set; }
-        public Phone ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
         public List<Dog> Dogs { get; set; }
         public List<Sitter> {get; set;}
         public Address Address { get; set; }
