e4009b1cec4d0a47a0e347b64e7ef8521fdf2f1d
NikPletnev <klophah@gmail.com>
2022-02-02 16:52:24 +0300

    created blank for API

---
+++ DogSitter/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using DogSitter.DAL.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogSitter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class CustomerController : Controller
+    {
+        [HttpGet("{id}")]
+        public ActionResult<Customer> GetCustomerById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+
+        public ActionResult<Customer> GetAllCustomer()
+        {
+            return Ok();
+        }
+
+
+        [HttpPost("{customer}")]
+
+        public ActionResult AddCustomer(Customer customer)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{customer}")]
+
+        public ActionResult UpdateCustomer(Customer customer)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+
+        public ActionResult DeleteCustomer(int id)
+        {
+            return Ok();
+        }
+
+    }
+
+}
