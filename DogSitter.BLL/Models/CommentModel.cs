5fcbaaffd9386e899e0964c1fe4c72dd51365eab
Olga <eov2700@gmail.com>
2022-02-03 19:30:10 +0300

    Added CommentService

---
+++ DogSitter.BLL/Models/CommentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSitter.BLL.Models
+{
+    public class CommentModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ DogSitter.BLL/Services/CommentService.cs
@@ -0,0 +1,96 @@
+﻿using DogSitter.BLL.Config;
+using DogSitter.BLL.Models;
+using DogSitter.DAL.Entity;
+using DogSitter.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSitter.BLL.Services
+{
+    public class CommentService
+    {
+        private readonly CommentRepository _repository;
+
+        public CommentService()
+        {
+            _repository = new CommentRepository();
+        }
+
+        public CommentModel GetById(int id)
+        {
+            try
+            {
+                var comment = _repository.GetById(id);
+                return CustomMapper.GetInstance().Map<CommentModel>(comment);
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Комментарий не найден");
+            }
+        }
+
+        public List<CommentModel> GetAll()
+        {
+            var comment = _repository.GetAll();
+            return CustomMapper.GetInstance().Map<List<CommentModel>>(comment);
+        }
+
+        public void Add(CommentModel commentModel)
+        {
+            var comment = CustomMapper.GetInstance().Map<Comment>(commentModel);
+            _repository.Add(comment);
+        }
+
+        public void Update(CommentModel commentModel)
+        {
+            var comment = CustomMapper.GetInstance().Map<Comment>(commentModel);
+            try
+            {
+                var entity = _repository.GetById(commentModel.Id);
+
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Комментарий не найден");
+            }
+            _repository.Update(comment);
+        }
+
+        public void DeleteById(int id)
+        {
+            try
+            {
+                var entity = _repository.GetById(id);
+
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Комментарий не найден");
+            }
+            bool delete = true;
+            _repository.Update(id, delete);
+        }
+
+        public void Restore(int id)
+        {
+            try
+            {
+                var entity = _repository.GetById(id);
+
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Комментарий не найден");
+            }
+            bool Delete = false;
+            _repository.Update(id, Delete);
+        }
+    }
+}
