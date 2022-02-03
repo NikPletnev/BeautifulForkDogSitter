07f59c79cd846f6e420175b172780e0400325ef1
Olga <eov2700@gmail.com>
2022-02-03 21:01:59 +0300

    Added API for Sitter

---
+++ DogSitter/Controllers/SitterController.cs
@@ -0,0 +1,44 @@
+﻿using DogSitter.BLL.Models;
+using DogSitter.BLL.Services;
+using DogSitter.DAL.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogSitter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SitterController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public ActionResult<Sitter> GetbyId(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult<List<Sitter>> GetAll(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Add(SitterModel sittetModel)
+        {            
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Update(SitterModel sitterModel)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
