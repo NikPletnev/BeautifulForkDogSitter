74db6bdc81fe6ee9ffc3ec768978dedc637f8b8e
Alyona <alianakass@icloud.com>
2022-02-10 22:44:54 +0300

    tests for contact service

---
+++ DogSitter.BLL.Tests/ContactServiceTests.cs
@@ -0,0 +1,171 @@
+﻿using AutoMapper;
+using DogSitter.BLL.Configs;
+using DogSitter.BLL.Exeptions;
+using DogSitter.BLL.Models;
+using DogSitter.BLL.Services;
+using DogSitter.BLL.Tests.TestCaseSource;
+using DogSitter.DAL.Entity;
+using DogSitter.DAL.Enums;
+using DogSitter.DAL.Repositories;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DogSitter.BLL.Tests
+{
+    public class ContactServiceTests
+    {
+        private readonly Mock<IContactRepository> _ContactRepositoryMock;
+        private readonly IMapper _mapper;
+        private readonly ContactService _service;
+
+        public ContactServiceTests()
+        {
+            _ContactRepositoryMock = new Mock<IContactRepository>();
+            _mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<CustomMapper>()));
+            _service = new ContactService(_ContactRepositoryMock.Object, _mapper);
+        }
+
+        [Test]
+        public void UpdateContactTest(int id, Contact entity, ContactModel model)
+        {
+            //given
+            _ContactRepositoryMock.Setup(x => x.UpdateContact(entity)).Verifiable();
+            _ContactRepositoryMock.Setup(x => x.GetContactById(id));
+            //when
+
+            //then
+            Assert.DoesNotThrow(() => _service.UpdateContact(id, model));
+            Assert.Pass();
+            _ContactRepositoryMock.Verify();
+            _ContactRepositoryMock.Verify(x => x.GetContactById(id), Times.Once);
+        }
+
+        [TestCase(1)]
+        [TestCase(11)]
+        [TestCase(100)]
+        public void UpdateContactTest_WhenNotEnoughDataAboutContact_ShouldServiceNotEnoughDataExeption(int id)
+        {
+            //given
+            var contactModel = new ContactModel() { Value = "" };
+
+            //when
+
+            //then
+
+            Assert.Throws<ServiceNotEnoughDataExeption>(() => _service.UpdateContact(id, contactModel));
+        }
+
+        [TestCase(1)]
+        [TestCase(11)]
+        [TestCase(100)]
+        public void UpdateContactTest_WhenContactNotFound_ShouldServiceNotFoundExeption(int id)
+        {
+            //given
+            _ContactRepositoryMock.Setup(x => x.UpdateContact(It.IsAny<Contact>()));
+            //when
+            //then
+            Assert.Throws<ServiceNotFoundExeption>(() => _service.UpdateContact(id, new ContactModel()));
+        }
+
+        [TestCaseSource(typeof(AddContactTestCaseSource))]
+        public void AddContactTest(ContactModel contact)
+        {
+            //given
+            _ContactRepositoryMock.Setup(x => x.AddContact(It.IsAny<Contact>())).Verifiable();
+            //when
+            //then
+            Assert.DoesNotThrow(() => _service.AddContact(contact));
+            Assert.Pass();
+            _ContactRepositoryMock.Verify();
+        }
+
+        [Test]
+        public void AddContactTest_WhenNotEnoughDataAboutContact_ShouldServiceNotFoundExeption()
+        {
+            //given
+            _ContactRepositoryMock.Setup(x => x.AddContact(It.IsAny<Contact>()));
+            var contact = new ContactModel() { Value = "" };
+            //when
+            //then
+            Assert.Throws<ServiceNotEnoughDataExeption>(() => _service.AddContact(contact));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(138)]
+        public void DeleteOrRestoreContactTest(int id)
+        {
+            //given
+            Contact contact = new Contact() {Id = id, Value = "123456", ContactType = ContactType.phone, IsDeleted = false };
+            _ContactRepositoryMock.Setup(x => x.UpdateContact(contact.Id, true)).Verifiable();
+            _ContactRepositoryMock.Setup(x => x.UpdateContact(contact.Id, false)).Verifiable();
+            _ContactRepositoryMock.Setup(x => x.GetContactById(contact.Id)).Returns(contact);
+            //when                     
+            //then
+            Assert.DoesNotThrow(() => _service.DeleteContact(id));
+            Assert.DoesNotThrow(() => _service.RestoreContact(id));
+            Assert.Pass();
+            _ContactRepositoryMock.VerifyAll();
+        }
+
+        [TestCase(2)]
+        [TestCase(100)]
+        [TestCase(11)]
+        public void DeleteOrRestoreContactTest_WhenContactNotFound_ShouldServiceNotFoundExeption(int id)
+        {
+            //given
+            Contact contact = new Contact() { Id = id, Value = "123456", ContactType = ContactType.phone, IsDeleted = false };
+            _ContactRepositoryMock.Setup(x => x.UpdateContact(contact.Id, true)).Verifiable();
+            _ContactRepositoryMock.Setup(x => x.UpdateContact(contact.Id, false)).Verifiable();
+            _ContactRepositoryMock.Setup(x => x.GetContactById(contact.Id));
+            //when
+
+            //then
+
+            Assert.Throws<ServiceNotFoundExeption>(() => _service.DeleteContact(id));
+            Assert.Throws<ServiceNotFoundExeption>(() => _service.RestoreContact(id));
+        }
+
+        [TestCaseSource(typeof(GetContactByIdTestCaseSource))]
+        public void GetContactByIdTest(int id, Contact contact)
+        {
+            //given
+            _ContactRepositoryMock.Setup(x => x.GetContactById(id)).Returns(contact).Verifiable();
+            //when
+
+            //then
+            Assert.DoesNotThrow(() => _service.GetContactById(id));
+            Assert.Pass();
+            _ContactRepositoryMock.Verify();
+        }
+
+        [TestCase(2)]
+        [TestCase(100)]
+        [TestCase(11)]
+        public void GetContactByIdTest_WhenContactNotFound_ShouldServiceNotFoundExeption(int id)
+        {
+            //given
+            _ContactRepositoryMock.Setup(x => x.GetContactById(id)).Verifiable();
+            //when
+
+            //then
+
+            Assert.Throws<ServiceNotFoundExeption>(() => _service.GetContactById(id));
+            _ContactRepositoryMock.Verify();
+        }
+
+        [TestCaseSource(typeof(GetAllContactTestCaseSource))]
+        public void GetAllContactsTest(List<Contact> contacts)
+        {
+            //given
+            _ContactRepositoryMock.Setup(x => x.GetAllContacts()).Returns(contacts).Verifiable();
+            //when
+
+            //then
+            Assert.DoesNotThrow(() => _service.GetAllContacts());
+            Assert.Pass();
+            _ContactRepositoryMock.Verify();
+        }
+    }
+}
---
+++ DogSitter.BLL.Tests/TestCaseSource/GetAllContactTestCaseSource.cs
@@ -0,0 +1,25 @@
+﻿using DogSitter.DAL.Entity;
+using DogSitter.DAL.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSitter.BLL.Tests.TestCaseSource
+{
+    public class GetAllContactTestCaseSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            List<Contact> contacts = new List<Contact>() {
+              new Contact() { Value = "89871234567", ContactType = ContactType.phone, IsDeleted = false },
+              new Contact() { Value = "@qwerty", ContactType = ContactType.mail, IsDeleted = false },
+              new Contact() { Value = "qwerty123@icloud.com", ContactType = ContactType.mail, IsDeleted = true }
+            };
+
+            yield return new object[] { contacts };
+        }
+    }
+}
