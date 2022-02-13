054801592d233b8aa5c6a9ef85059dd8df63eedb
Alyona <alianakass@icloud.com>
2022-02-14 00:47:58 +0300

    WIP

--- DogSitter.BLL/Models/OrderModel.cs
+++ DogSitter.BLL/Models/OrderModel.cs
@@ -1,6 +1,20 @@
-﻿namespace DogSitter.BLL.Models
+﻿using DogSitter.DAL.Entity;
+using DogSitter.DAL.Enums;
+
+namespace DogSitter.BLL.Models
 {
     public class OrderModel
     {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Price { get; set; }
+        public Status Status { get; set; }
+        public int? Mark { get; set; }
+        public bool IsDeleted { get; set; }
+        public CustomerModel Customer { get; set; }
+        public SitterModel Sitter { get; set; }
+        public Comment Comment { get; set; }
+        public DogModel Dogs { get; set; }
+        public virtual List<ServiceModel> Service { get; set; }
     }
 }
