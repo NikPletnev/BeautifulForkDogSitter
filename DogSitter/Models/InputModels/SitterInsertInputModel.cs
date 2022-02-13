b0496c936dd4bdd89c4cdfdf478113d8363ca051
Alyona <alianakass@icloud.com>
2022-02-13 23:17:30 +0300

    sitter insert model

--- DogSitter/Models/InputModels/SitterInsertInputModel.cs
+++ DogSitter/Models/InputModels/SitterInsertInputModel.cs
@@ -5,5 +5,10 @@
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public List<ContactInsertInputModel> Contacts { get; set; }
+        public PassportInsertInputModel Passport { get; set; }
+        public SubwayStationInputModel SubwayStation { get; set; }
+        public string Information { get; set; }
+
     }
 }
