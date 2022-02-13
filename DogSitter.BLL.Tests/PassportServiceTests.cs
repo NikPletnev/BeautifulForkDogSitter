74093678950a2ae8a0361adf7e8eb066c15d3293
Alyona <alianakass@icloud.com>
2022-02-13 19:08:29 +0300

    minor change

--- DogSitter.BLL.Tests/PassportServiceTests.cs
+++ DogSitter.BLL.Tests/PassportServiceTests.cs
@@ -14,15 +14,15 @@ namespace DogSitter.BLL.Tests
 {
     public class PassportServiceTests
     {
-        private readonly Mock<IPassportRepository> _PassportRepositoryMock;
+        private readonly Mock<IPassportRepository> _passportRepositoryMock;
         private readonly IMapper _mapper;
         private readonly PassportService _service;
 
         public PassportServiceTests()
         {
-            _PassportRepositoryMock = new Mock<IPassportRepository>();
+            _passportRepositoryMock = new Mock<IPassportRepository>();
             _mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<CustomMapper>()));
-            _service = new PassportService(_PassportRepositoryMock.Object, _mapper);
+            _service = new PassportService(_passportRepositoryMock.Object, _mapper);
         }
 
 
@@ -30,22 +30,22 @@ namespace DogSitter.BLL.Tests
         public void UpdatePassportTest(int id, Passport entity, PassportModel model)
         {
             //given
-            _PassportRepositoryMock.Setup(x => x.UpdatePassport(entity)).Verifiable();
-            _PassportRepositoryMock.Setup(x => x.GetPassportById(id)).Returns(entity);
+            _passportRepositoryMock.Setup(x => x.UpdatePassport(entity)).Verifiable();
+            _passportRepositoryMock.Setup(x => x.GetPassportById(id)).Returns(entity);
             //when       
             _service.UpdatePassport(id, model);
             //then
             Assert.AreEqual(model.IsDeleted, entity.IsDeleted);
-            _PassportRepositoryMock.Verify();
-            _PassportRepositoryMock.Verify(x => x.GetPassportById(id), Times.Once);
-            _PassportRepositoryMock.Verify(x => x.UpdatePassport(entity), Times.Once);
+            _passportRepositoryMock.Verify();
+            _passportRepositoryMock.Verify(x => x.GetPassportById(id), Times.Once);
+            _passportRepositoryMock.Verify(x => x.UpdatePassport(entity), Times.Once);
         }
 
         [TestCase(100)]
         public void UpdatePassportTest_WhenPassportNotFound_ShouldThrowServiceNotFoundExeption(int id)
         {
             //given
-            _PassportRepositoryMock.Setup(x => x.UpdatePassport(It.IsAny<Passport>()));
+            _passportRepositoryMock.Setup(x => x.UpdatePassport(It.IsAny<Passport>()));
             //when
             //then
             Assert.Throws<ServiceNotFoundExeption>(() => _service.UpdatePassport(id, new PassportModel()));
@@ -95,11 +95,11 @@ namespace DogSitter.BLL.Tests
                 IsDeleted = false
             };
 
-            _PassportRepositoryMock.Setup(x => x.AddPassport(It.IsAny<Passport>())).Verifiable();
+            _passportRepositoryMock.Setup(x => x.AddPassport(It.IsAny<Passport>())).Verifiable();
             //when         
              _service.AddPassport(model);
             //then
-            _PassportRepositoryMock.Verify();
+            _passportRepositoryMock.Verify();
         }
 
         [TestCaseSource(typeof(AddPassportNegativeTestCaseSource))]
@@ -115,23 +115,23 @@ namespace DogSitter.BLL.Tests
         public void GetPassportByIdTest(int id, Passport entity, PassportModel expected )
         {
             //given
-            _PassportRepositoryMock.Setup(x => x.GetPassportById(id)).Returns(entity).Verifiable();
+            _passportRepositoryMock.Setup(x => x.GetPassportById(id)).Returns(entity).Verifiable();
             //when
             PassportModel actual = _service.GetPassportById(id);
             //then
             Assert.AreEqual(expected, actual);
-            _PassportRepositoryMock.Verify();
+            _passportRepositoryMock.Verify();
         }
 
         [TestCase(11)]
         public void GetPassportByIdTest_WhenPassportNotFound_ShouldThrowServiceNotFoundExeption(int id)
         {
             //given
-            _PassportRepositoryMock.Setup(x => x.GetPassportById(id)).Verifiable();
+            _passportRepositoryMock.Setup(x => x.GetPassportById(id)).Verifiable();
             //when
             //then
             Assert.Throws<ServiceNotFoundExeption>(() => _service.GetPassportById(id));
-            _PassportRepositoryMock.Verify();
+            _passportRepositoryMock.Verify();
         }
     }
 }
