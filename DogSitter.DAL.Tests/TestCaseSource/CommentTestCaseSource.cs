00f2db32df03f9aea3c748762f03cb8e09854569
bnmug <eov2700@gmail.com>
2022-02-14 20:21:13 +0300

    Merge DEV into DOGSIT-28

---
+++ DogSitter.DAL.Tests/TestCaseSource/CommentTestCaseSource.cs
@@ -0,0 +1,25 @@
+﻿using DogSitter.DAL.Entity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSitter.DAL.Tests.TestCaseSource
+{
+    public class CommentTestCaseSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            List<Comment> comments = new List<Comment>()
+            {
+                new Comment(){ Text = "Test text ONE", Date = DateTime.Now, IsDeleted = false},
+                new Comment(){ Text = "Test text TWO", Date = DateTime.Now, IsDeleted = true}
+            };
+
+            yield return new object[] { comments };
+
+        }
+    }
+}
