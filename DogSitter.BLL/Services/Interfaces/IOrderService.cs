6944e2256d37cf8bd3ef41e64d8b634b8e2ba2d6
bnmug <eov2700@gmail.com>
2022-02-18 19:16:12 +0300

    added part API for Order

--- DogSitter.BLL/Services/Interfaces/IOrderService.cs
+++ DogSitter.BLL/Services/Interfaces/IOrderService.cs
@@ -1,7 +1,15 @@
-﻿namespace DogSitter.BLL.Services
+﻿using DogSitter.BLL.Models;
+
+namespace DogSitter.BLL.Services.Interface
 {
     public interface IOrderService
     {
+        void Add(OrderModel orderModel);
+        void DeleteById(int id);
         void EditOrderStatusByOrderId(int id, int status);
+        List<OrderModel> GetAll();
+        OrderModel GetById(int id);
+        void Restore(int id);
+        void Update(OrderModel orderModel);
     }
 }
--- DogSitter.BLL/Services/OrderService.cs
+++ DogSitter.BLL/Services/OrderService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using DogSitter.BLL.Exeptions;
 using DogSitter.BLL.Models;
+using DogSitter.BLL.Services.Interface;
+using DogSitter.DAL.Entity;
 using DogSitter.DAL.Enums;
 using DogSitter.DAL.Repositories;
 using System;
@@ -13,23 +15,87 @@ namespace DogSitter.BLL.Services
 {
     public class OrderService : IOrderService
     {
-        private readonly IOrderRepository _rep;
-        private IMapper _map;
+        private readonly IOrderRepository _repository;
+        private IMapper _mapper;
 
         public OrderService(IOrderRepository orderRepository, IMapper mapper)
         {
-            _rep = orderRepository;
-            _map = mapper;
+            _repository = orderRepository;
+            _mapper = mapper;
         }
-
-        public void EditOrderStatusByOrderId(int id, int status)
+        public OrderModel GetById(int id)
         {
-            var order = _rep.GetById(id);
+            var order = _repository.GetById(id);
+
             if (order == null)
             {
                 throw new EntityNotFoundException($"Order {id} was not found");
             }
-            _rep.EditOrderStatusByOrderId(order, status);
+
+            return _mapper.Map<OrderModel>(order);
+        }
+
+        public List<OrderModel> GetAll() =>
+             _mapper.Map<List<OrderModel>>(_repository.GetAll());
+
+        public void Add(OrderModel orderModel)
+        {
+            if (orderModel.OrderDate == DateTime.MinValue ||
+                orderModel.Price == 0 ||
+                orderModel.Status == 0 ||
+                orderModel.Mark == null)
+            {
+                throw new ServiceNotEnoughDataExeption($"There is not enough data to create new order");
+            }
+
+            _repository.Add(_mapper.Map<Order>(orderModel));
+        }
+
+        public void Update(OrderModel orderModel)
+        {
+            if (orderModel.Price == 0 ||
+                orderModel.Status == 0 ||
+                orderModel.Mark == null)
+            {
+                throw new ServiceNotEnoughDataExeption($"There is not enough data to edit the order {orderModel.Id}");
+            }
+            var order = _repository.GetById(orderModel.Id);
+            if (order == null)
+            {
+                throw new EntityNotFoundException($"Order {orderModel.Id} was not found");
+            }
+            _repository.Update(order);
+        }
+
+        public void DeleteById(int id)
+        {
+            bool IsDelete = true;
+            var order = _repository.GetById(id);
+            if (order == null)
+            {
+                throw new EntityNotFoundException($"Order {id} was not found");
+            }
+            _repository.Update(id, IsDelete);
+        }
+
+        public void Restore(int id)
+        {
+            bool IsDelete = false;
+            var order = _repository.GetById(id);
+            if (order == null)
+            {
+                throw new EntityNotFoundException($"Order {id} was not found");
+            }
+            _repository.Update(id, IsDelete);
+        }
+        public void EditOrderStatusByOrderId(int id, int status)
+        {
+            var order = _repository.GetById(id);
+            if (order == null)
+            {
+                throw new EntityNotFoundException($"Order {id} was not found");
+            }
+            _repository.EditOrderStatusByOrderId(order, status);
         }
     }
 }
