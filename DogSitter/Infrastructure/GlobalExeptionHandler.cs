0014112a78bf2414bc616cee62e58918078692ee
Alyona <alianakass@icloud.com>
2022-02-22 16:33:22 +0300

    add migration

--- DogSitter/Infrastructure/GlobalExeptionHandler.cs
+++ DogSitter/Infrastructure/GlobalExeptionHandler.cs
@@ -18,6 +18,10 @@ namespace DogSitter.API.Infrastructure
             try
             {
                 await _next(context);
+            }          
+            catch (AccessException ex)
+            {
+                await HandleExceptionAsync(context, HttpStatusCode.NotFound, ex.Message);
             }
             catch (EntityNotFoundException ex)
             {
