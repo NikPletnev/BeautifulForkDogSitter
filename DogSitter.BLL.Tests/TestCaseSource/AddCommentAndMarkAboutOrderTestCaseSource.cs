c1ebd87735af9394f4cbc2151f521b1b29429235
NikPletnev <klophah@gmail.com>
2022-02-22 12:54:53 +0300

    fix case source

--- DogSitter.BLL.Tests/TestCaseSource/AddCommentAndMarkAboutOrderTestCaseSource.cs
+++ DogSitter.BLL.Tests/TestCaseSource/AddCommentAndMarkAboutOrderTestCaseSource.cs
@@ -34,7 +34,7 @@ namespace DogSitter.BLL.Tests.TestCaseSource
                 IsDeleted = false
             };
 
-            int idSitter = sitter.Id;
+            int SitterId = sitter.Id;
             Order order = new Order()
             {
                 Id = 3,
@@ -42,11 +42,10 @@ namespace DogSitter.BLL.Tests.TestCaseSource
                 Price = 100,
                 Status = Status.Completed,
                 Sitter = sitter,
-                //Comment = new Comment() { Id = 1, Text = "All right", Date = new DateTime(2000, 11, 11), IsDeleted = false },
                 IsDeleted = false
             };
 
-            int idOrder = order.Id;
+            int OrderId = order.Id;
 
 
             List<Order> orders = new List<Order>()
@@ -86,7 +85,7 @@ namespace DogSitter.BLL.Tests.TestCaseSource
             };
 
 
-            yield return new object[] { order, idOrder, idSitter, sitter, orders, orderModel };
+            yield return new object[] { order, OrderId, SitterId, sitter, orders, orderModel };
         }
     }
 
