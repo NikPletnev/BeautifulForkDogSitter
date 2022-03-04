f337358e358706f2508cae8930732f3e68ad93fb
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-03-04 23:18:30 +0300

    minor change

--- DogSitter/Models/InputModels/DogUpdateInputModel.cs
+++ DogSitter/Models/InputModels/DogUpdateInputModel.cs
@@ -8,10 +8,10 @@ namespace DogSitter.API.Models
         [RegularExpression(@"^([а-яёА-ЯЁ\s]+|[a-zA-Z\s]+)$")]
         public string Name { get; set; }
         [Required]
-        [Range(0, 10000, ErrorMessage = "Недопустимый возраст")]
+        [Range(0, 30, ErrorMessage = "Недопустимый возраст")]
         public int Age { get; set; }
         [Required]
-        [Range(0, 10000, ErrorMessage = "Недопустимый вес")]
+        [Range(0, 50, ErrorMessage = "Недопустимый вес")]
         public double Weight { get; set; }
         [RegularExpression(@"^([а-яёА-ЯЁ\s]+|[a-zA-Z\s]+)$")]
         public string Description { get; set; }
