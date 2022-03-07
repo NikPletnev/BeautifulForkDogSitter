dd8e3453ab42089ac0d72940b186cbed14d1d1eb
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-03-07 18:51:00 +0300

    bugfix DateOfBirth

---
+++ DogSitter/Attribute/SitterMinAge.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DogSitter.API.Attribute
+{
+    public class SitterMinAge : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime bday = DateTime.Parse(value.ToString());
+            DateTime today = DateTime.Today;
+            int age = today.Year - bday.Year;
+            if (bday > today.AddYears(-age))
+            {
+                age--;
+            }
+            if (age < 16)
+            {
+                var result = new ValidationResult("Sorry, you are not old enough");
+                return result;
+            }
+            
+            return null;
+        }
+    }
+}
+
--- DogSitter/Models/InputModels/PassportUpdateInputModel.cs
+++ DogSitter/Models/InputModels/PassportUpdateInputModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using DogSitter.API.Attribute;
+using System.ComponentModel.DataAnnotations;
 
 namespace DogSitter.API.Models
 {
@@ -7,28 +8,35 @@ namespace DogSitter.API.Models
         [Required]
         [RegularExpression(@"^([а-яёА-ЯЁ\s]+|[a-zA-Z\s]+)$")]
         public string FirstName { get; set; }
+
         [Required]
         [RegularExpression(@"^([а-яёА-ЯЁ\s]+|[a-zA-Z\s]+)$")]
         public string LastName { get; set; }
+
         [Required]
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
+        [SitterMinAge]
         public DateTime DateOfBirth { get; set; }
+
         [Required]
         [RegularExpression(@"^([1-9]\d*)?\d$")]
         public string Seria { get; set; }
+
         [Required]
         [RegularExpression(@"^([1-9]\d*)?\d$")]
         public string Number { get; set; }
+
         [Required]
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
         public DateTime IssueDate { get; set; }
+
         [Required]
         [RegularExpression(@"^([а-яёА-ЯЁ\s]+|[a-zA-Z\s]+)$")]
         public string Division { get; set; }
+
         [Required]
         [RegularExpression(@"^([1-9]\d*)?\d$")]
+
         public string DivisionCode { get; set; }
         public string Registration { get; set; }
     }
