3a558fd6899ec244a10b388fc54d4559ac366630
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-03-15 12:58:13 +0300

    add migration

--- DogSitter.Data/Migrations/20220315095412_Change House number from int to string.Designer.cs
+++ DogSitter.Data/Migrations/20220315095412_Change House number from int to string.Designer.cs
@@ -4,6 +4,7 @@ using DogSitter.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DogSitter.DAL.Migrations
 {
     [DbContext(typeof(DogSitterContext))]
-    partial class DogSitterContextModelSnapshot : ModelSnapshot
+    [Migration("20220315095412_Change House number from int to string")]
+    partial class ChangeHousenumberfrominttostring
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
